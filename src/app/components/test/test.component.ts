5f963844911cf5238a864f796cd16c2aa05b346b
Fredrik Hegstam <fredrikhegstam@gmail.com>
2019-12-07 13:57:06 +0100

    table implementation
    Implementation of ngx-datatable in mobile app with light/dark-mode switch etc.

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+})
+export class TestComponent implements OnInit {
+
+  // data: Observable<any>;
+
+  constructor(private http: HttpClient) { }
+
+  private userData = new BehaviorSubject(null);
+
+
+  login(credentials: { email: string, pw: string }): Observable<any> {
+    this.http.get('https://randomuser.me/api/').pipe(
+      map(data => {
+        this.userData.next(data);
+        // return this.userData.getValue();
+      })
+    );
+  }
+
+  getUser() {
+    return this.userData.getValue();
+  }
+
+  logout() {
+    this.userData.next(null);
+  }
+
+  ngOnInit() { }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import data from './../../assets/company.json';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private companies: any[] = data;
+  tablestyle = 'bootstrap';
+  customRowClass = false;
+
+  constructor(private alertCtrl: AlertController) { }
+
+  switchStyle() {
+    if (this.tablestyle == 'dark') {
+      this.tablestyle = 'bootstrap';
+    } else {
+      this.tablestyle = 'dark';
+    }
+  }
+
+  getRowClass(row) {
+    const isMale = row.gender == 'male';
+    if (!this.customRowClass) {
+      return {};
+    }
+    return {
+      'male-row': isMale,
+      'female-row': !isMale
+    }
+  }
+
+  async open(row) {
+    let alert = await this.alertCtrl.create({
+      header: 'Row',
+      message: `${row.name} works for ${row.company}!`,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 
 }
